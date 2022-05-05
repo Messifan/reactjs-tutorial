ac57db6d4340e8a2e0f3d4918602706e01caba02
Fan Dong <fandong@Fans-MacBook-Pro.local>
2022-05-05 12:38:38 -0500

    add notes in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//ReactDom: rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//add <App /> to <div id="root"></div> in public/index.html
 root.render(
   <React.StrictMode>
     <App />
