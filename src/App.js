d3c44c5b83cd66ee8a0fc39b8dcffb31e44612ee
Fan Dong <fandong@Fans-MacBook-Pro.local>
2022-05-05 13:40:55 -0500

    add jsx notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import JsExpression from "./jsx/jsx_expression"
 function App() {
   return (
     <div className="App">
       <h1>First React App</h1>
+      <JsExpression />
     </div>
   );
 }
---
+++ src/jsx/jsx_expression.js
@@ -0,0 +1,64 @@
+import './jsx_expression.css'
+//define variables
+const name = 'big john'
+
+const getAge = () => {
+    return 18
+}
+
+
+
+//map render;
+//list need a unpeated number/string key to improve performance
+const songs = [
+    { id: 1, name: 'first song' },
+    { id: 2, name: 'second song' },
+    { id: 3, name: 'third song' },
+    // {id: 1, name: 'forth song'}
+]
+
+//conditional render: ternary expression 
+//&&
+const flag = true
+
+//type condition: 1 2 3
+//show red h1; green h2; blue h3
+const style = {
+    color: 'red',
+    fontSize: '30px'
+}
+
+const h3activeFlag = true
+
+const getHtag = (type) => {
+    if (type === 1) {
+        return <h1 style={style}> this is h1</h1>
+    }
+    if (type === 2) {
+        return <h2 style={{ color: 'green' }}> this is h2 </h2>
+    }
+    if (type === 3) {
+        return <h3 className={h3activeFlag ? 'h3active' : ''}> this is h3</h3>
+    }
+}
+
+//jsx: className <- class; htmlFor <- for;
+
+function JsExpression () {
+    return (
+        <div>
+            {name}
+            {getAge()}
+            <ul>
+                {songs.map(s => <li key={s.id}> {s.name} </li>)}
+            </ul>
+            {flag ? (<span>flag = true span</span>) : null}
+            {false && (<span>false span not show</span>)}
+            {getHtag(1)}
+            {getHtag(2)}
+            {getHtag(3)}
+
+        </div>
+    )
+}
+export default JsExpression
